55ba30769bd9dac6c6bf02910d1789186c616a8d
leehaneum <leehaneum160924@kyonggi.ac.kr>
2025-11-28 22:42:19 +0900

    fix(cd): properties 바인딩 문제, jpa 엔티티 스캔 문제 해결

--- gateway/src/main/kotlin/tamago/server/gateway/TamaGoApplication.kt
+++ gateway/src/main/kotlin/tamago/server/gateway/TamaGoApplication.kt
@@ -1,8 +1,10 @@
 package tamago.server.gateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 class TamaGoApplication
 
