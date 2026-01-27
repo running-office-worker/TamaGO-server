c8543de7749e0805093f8cbee551df6247484694
leehaneum <leehaneum160924@kyonggi.ac.kr>
2026-01-27 16:28:42 +0900

    feat: ktlint formatting

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
-	kotlin("jvm") version "1.9.25"
-	kotlin("plugin.spring") version "1.9.25"
-	kotlin("plugin.jpa") version "1.9.25"
-	id("org.springframework.boot") version "3.5.8"
-	id("io.spring.dependency-management") version "1.1.7"
-	id("org.jlleitschuh.gradle.ktlint") version "14.0.1"
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    kotlin("plugin.jpa") version "1.9.25"
+    id("org.springframework.boot") version "3.5.8"
+    id("io.spring.dependency-management") version "1.1.7"
+    id("org.jlleitschuh.gradle.ktlint") version "14.0.1"
 }
 
 group = "tamago"
@@ -16,47 +16,73 @@ description = "TamaGo Server"
 val springModulithVersion = "1.4.3"
 
 subprojects {
-	apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "io.spring.dependency-management")
 
-	repositories {
-		mavenCentral()
-	}
+    repositories {
+        mavenCentral()
+    }
 
-	dependencyManagement {
-		imports {
-			mavenBom("org.springframework.modulith:spring-modulith-bom:$springModulithVersion")
-		}
-	}
+    dependencyManagement {
+        imports {
+            mavenBom("org.springframework.modulith:spring-modulith-bom:$springModulithVersion")
+        }
+    }
 
-	plugins.withType<JavaPlugin> {
-		extensions.configure<JavaPluginExtension> {
-			toolchain {
-				languageVersion.set(JavaLanguageVersion.of(21))
-			}
-		}
-	}
+    plugins.withType<JavaPlugin> {
+        extensions.configure<JavaPluginExtension> {
+            toolchain {
+                languageVersion.set(JavaLanguageVersion.of(21))
+            }
+        }
+    }
 }
 
 configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
-	debug.set(true)
-	verbose.set(true)
-	outputToConsole.set(true)
-	ignoreFailures.set(false)
-	filter {
-		exclude("**/generated/**")
-	}
+    debug.set(true)
+    verbose.set(true)
+    outputToConsole.set(true)
+    ignoreFailures.set(false)
+    filter {
+        exclude("**/generated/**")
+    }
 }
 
 java {
-	toolchain {
-		languageVersion.set(JavaLanguageVersion.of(21))
-	}
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 tasks.withType<BootJar> {
-	mainClass.set("tamago.server.gateway.TamaGoApplicationKt")
+    mainClass.set("tamago.server.gateway.TamaGoApplicationKt")
+}
+
+tasks.register<Copy>("installGitHooks") {
+    group = "git hooks"
+    description = "Install pre-commit git hook for ktlint check"
+    from("${rootProject.rootDir}/scripts/pre-commit")
+    into("${rootProject.rootDir}/.git/hooks")
+    filePermissions {
+        user {
+            read = true
+            write = true
+            execute = true
+        }
+        group {
+            read = true
+            execute = true
+        }
+        other {
+            read = true
+            execute = true
+        }
+    }
+}
+
+tasks.named("compileKotlin") {
+    dependsOn("installGitHooks")
 }
