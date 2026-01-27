68ebf83268c5ce8dc6e4102e79ceb7d1c6aedb33
leehaneum <leehaneum160924@kyonggi.ac.kr>
2026-01-28 00:58:54 +0900

    feat: notification 모듈 추가

---
+++ notification/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.spring")
+    kotlin("plugin.jpa")
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+}
+
+dependencies {
+    implementation("org.springframework.modulith:spring-modulith-starter-core")
+}
+
+tasks.named<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
+    enabled = false
+}
+
+tasks.named<Jar>("jar") {
+    enabled = true
+}
