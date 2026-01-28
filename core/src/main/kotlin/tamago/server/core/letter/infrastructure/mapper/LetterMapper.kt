e9ad1e59dd69b0b0c02dd01615f01f065202f737
leehaneum <leehaneum160924@kyonggi.ac.kr>
2026-01-28 17:44:02 +0900

    feat: 편지 엔티티 모델 및 매퍼 구현

---
+++ core/src/main/kotlin/tamago/server/core/letter/infrastructure/mapper/LetterMapper.kt
@@ -0,0 +1,29 @@
+package tamago.server.core.letter.infrastructure.mapper
+
+import tamago.server.core.letter.domain.aggregate.Letter
+import tamago.server.core.letter.domain.vo.LetterContent
+import tamago.server.core.letter.domain.vo.LetterId
+import tamago.server.core.letter.infrastructure.entity.LetterEntity
+
+object LetterMapper {
+    fun toEntity(letter: Letter): LetterEntity {
+        return LetterEntity(
+            id = letter.id?.value,
+            title = letter.title,
+            content = letter.content.value,
+        )
+    }
+
+    fun toDomain(entity: LetterEntity?): Letter? {
+        if (entity == null) return null
+
+        return Letter(
+            id = entity.id?.let { LetterId(it) },
+            title = entity.title,
+            content = LetterContent(entity.content),
+            createdAt = entity.createdAt,
+            updatedAt = entity.updatedAt,
+            deletedAt = entity.deletedAt,
+        )
+    }
+}
