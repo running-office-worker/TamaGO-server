596f59ac9065ad03a65841cf9fb4244a580b1858
leehaneum <leehaneum160924@kyonggi.ac.kr>
2026-02-06 19:46:08 +0900

    chore: Flyway 마이그레이션 무결성 검증 추가

--- tamago-core/src/test/kotlin/tamago/server/core/TestApplication.kt
+++
@@ -1,6 +0,0 @@
-package tamago.server.core
-
-import org.springframework.boot.autoconfigure.SpringBootApplication
-
-@SpringBootApplication(scanBasePackages = ["tamago.server.core"])
-class TestApplication
