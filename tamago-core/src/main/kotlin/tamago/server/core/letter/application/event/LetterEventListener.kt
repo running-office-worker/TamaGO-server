b9a0aa68f1947211a24c53063d7de689bdf33d12
leehaneum <leehaneum160924@kyonggi.ac.kr>
2026-02-16 16:59:20 +0900

    feat: 이벤트 리스너에 주석 추가

--- tamago-core/src/main/kotlin/tamago/server/core/letter/application/event/LetterEventListener.kt
+++ tamago-core/src/main/kotlin/tamago/server/core/letter/application/event/LetterEventListener.kt
@@ -18,14 +18,17 @@ class LetterEventListener(
     @Transactional
     @TransactionalEventListener
     fun onRunningCompleted(event: RunningCompletedEvent) {
+        // 이미 스케줄된 편지가 있는 경우, 편지 발송 시간을 재조정
         userLetterPersistencePort.findByUserIdAndLetterStatus(event.userId, LetterStatus.SCHEDULED)?.let {
             it.reschedule(LocalDateTime.now())
             userLetterPersistencePort.save(it)
             return
         }
 
+        // 스케줄된 편지가 없는 경우, 새로운 편지 생성
         val selectedLetter = letterQueryService.getRandomTemplate()
 
+        // 러닝 시작 시간 기준 23시간 후로 편지 발송 시간 설정
         val createdLetter = UserLetter.new(
             userId = event.userId,
             letterId = selectedLetter.id!!,
