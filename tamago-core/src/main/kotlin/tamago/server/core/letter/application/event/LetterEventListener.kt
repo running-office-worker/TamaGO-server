10601a92960472ee052857d0e6f4c028e01fa159
leehaneum <leehaneum160924@kyonggi.ac.kr>
2026-02-16 18:51:05 +0900

    refactor: LetterEventListener 새로운 트랜잭션에서 실행하고 예외처리

--- tamago-core/src/main/kotlin/tamago/server/core/letter/application/event/LetterEventListener.kt
+++ tamago-core/src/main/kotlin/tamago/server/core/letter/application/event/LetterEventListener.kt
@@ -1,6 +1,8 @@
 package tamago.server.core.letter.application.event
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Propagation
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.transaction.event.TransactionalEventListener
 import tamago.server.core.running.domain.event.RunningCompletedEvent
@@ -10,30 +12,37 @@ import tamago.server.core.letter.domain.enum.LetterStatus
 import tamago.server.core.letter.domain.port.outbound.UserLetterPersistencePort
 import java.time.LocalDateTime
 
+private val logger = KotlinLogging.logger {}
+
 @Component
 class LetterEventListener(
     private val letterQueryService: LetterQueryService,
     private val userLetterPersistencePort: UserLetterPersistencePort,
 ) {
-    @Transactional
     @TransactionalEventListener
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     fun onRunningCompleted(event: RunningCompletedEvent) {
-        // 이미 스케줄된 편지가 있는 경우, 편지 발송 시간을 재조정
-        userLetterPersistencePort.findByUserIdAndLetterStatus(event.userId, LetterStatus.SCHEDULED)?.let {
-            it.reschedule(LocalDateTime.now())
-            userLetterPersistencePort.save(it)
-            return
+        try {
+            // 이미 스케줄된 편지가 있는 경우, 편지 발송 시간을 재조정
+            userLetterPersistencePort.findByUserIdAndLetterStatus(event.userId, LetterStatus.SCHEDULED)?.let {
+                it.reschedule(LocalDateTime.now())
+                userLetterPersistencePort.save(it)
+                return
+            }
+
+            // 스케줄된 편지가 없는 경우, 새로운 편지 생성
+            val selectedLetter = letterQueryService.getRandomTemplate()
+
+            // 러닝 시작 시간 기준 23시간 후로 편지 발송 시간 설정
+            val createdLetter = UserLetter.new(
+                userId = event.userId,
+                letterId = selectedLetter.id!!,
+                startedAt = event.startedAt,
+            )
+            userLetterPersistencePort.save(createdLetter)
+        } catch (e: Exception) {
+            // TODO: Sentry 로 예외 전송
+            logger.error(e) { "러닝 완료 후 편지 발송 시간 조정 오류 - userId: ${event.userId}" }
         }
-
-        // 스케줄된 편지가 없는 경우, 새로운 편지 생성
-        val selectedLetter = letterQueryService.getRandomTemplate()
-
-        // 러닝 시작 시간 기준 23시간 후로 편지 발송 시간 설정
-        val createdLetter = UserLetter.new(
-            userId = event.userId,
-            letterId = selectedLetter.id!!,
-            startedAt = event.startedAt,
-        )
-        userLetterPersistencePort.save(createdLetter)
     }
 }
