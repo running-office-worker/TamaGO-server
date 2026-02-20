9c09c16db96956f2d27b4ed17e13fb10362984ae
leehaneum <leehaneum160924@kyonggi.ac.kr>
2026-02-20 17:52:02 +0900

    fix: S3Exception 생성자 오류 해결

--- tamago-aws/src/main/kotlin/tamago/server/aws/exception/S3Exception.kt
+++ tamago-aws/src/main/kotlin/tamago/server/aws/exception/S3Exception.kt
@@ -2,6 +2,8 @@ package tamago.server.aws.exception
 
 import tamago.server.core.common.exception.BusinessException
 
-class S3Exception: BusinessException(
-    AwsExceptionCode.S3_ERROR
-)
+class S3Exception(cause: Throwable? = null) : BusinessException(AwsExceptionCode.S3_ERROR) {
+    init {
+        cause?.let { initCause(it) }
+    }
+}
