37ac2d533972d5f6befe6ccd6e7b6460183f3f1f
leehaneum <leehaneum160924@kyonggi.ac.kr>
2026-03-10 19:19:06 +0900

    refactor: 빈 리스트 반환 시 예외 처리

--- tamago-core/src/main/kotlin/tamago/server/core/letter/application/service/LetterQueryService.kt
+++ tamago-core/src/main/kotlin/tamago/server/core/letter/application/service/LetterQueryService.kt
@@ -3,6 +3,7 @@ package tamago.server.core.letter.application.service
 import org.springframework.stereotype.Service
 import tamago.server.core.common.vo.UserId
 import tamago.server.core.letter.LetterQueryUseCase
+import tamago.server.core.letter.application.exception.LetterNotFoundException
 import tamago.server.core.letter.domain.aggregate.Letter
 import tamago.server.core.letter.domain.enum.LetterTag
 import tamago.server.core.letter.domain.port.inbound.query.LetterInboxDto
@@ -25,15 +26,11 @@ class LetterQueryService(
         }
     }
 
-    fun getRandomTemplate(): Letter = letterPersistencePort.findAllActive().random()
+    fun getRandomTemplate(): Letter =
+        letterPersistencePort.findAllActive().randomOrNull()
+            ?: throw LetterNotFoundException()
 
-    fun getRandomTemplateByTag(tag: LetterTag): Letter {
-        val letters = letterPersistencePort.findActiveByTag(tag)
-
-        if (letters.isEmpty()) {
-            return getRandomTemplate()
-        }
-
-        return letters.random()
-    }
+    fun getRandomTemplateByTag(tag: LetterTag): Letter =
+        letterPersistencePort.findActiveByTag(tag).randomOrNull()
+            ?: getRandomTemplate()
 }
