8ab64b376a351f29c648bef0aab304620c07ef8c
leehaneum <leehaneum160924@kyonggi.ac.kr>
2026-03-10 13:19:34 +0900

    feat: LetterTag 세분화

--- tamago-core/src/main/kotlin/tamago/server/core/letter/domain/enum/LetterTag.kt
+++ tamago-core/src/main/kotlin/tamago/server/core/letter/domain/enum/LetterTag.kt
@@ -1,6 +1,14 @@
 package tamago.server.core.letter.domain.enum
 
 enum class LetterTag {
-    INACTIVE_SHORT,
-    INACTIVE_MEDIUM,
+    FIRST_START,
+    INACTIVE_0_3,
+    INACTIVE_4_7,
+    INACTIVE_8_10,
+    INACTIVE_11_14,
+    INACTIVE_15_PLUS,
+    STREAK_1_2,
+    STREAK_3_5,
+    STREAK_6_9,
+    STREAK_10_PLUS,
 }
