ebaa2551d7e3cf2d4a8da5add8b66fbceccdad41
leehaneum <leehaneum160924@kyonggi.ac.kr>
2026-03-18 18:48:54 +0900

    feat: 시드 데이터에 기본 지급 몬스터 명시하고 연관된 쿼리 단순화

--- tamago-storage/src/main/kotlin/tamago/server/storage/monster/repository/MonsterJpaRepository.kt
+++ tamago-storage/src/main/kotlin/tamago/server/storage/monster/repository/MonsterJpaRepository.kt
@@ -1,8 +1,11 @@
 package tamago.server.storage.monster.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import tamago.server.core.monster.domain.enum.MonsterType
 import tamago.server.storage.monster.entity.MonsterEntity
 
 interface MonsterJpaRepository : JpaRepository<MonsterEntity, Long> {
     fun findAllByPreviousMonsterIsNull(): List<MonsterEntity>
+
+    fun findByMonsterType(monsterType: MonsterType): MonsterEntity?
 }
